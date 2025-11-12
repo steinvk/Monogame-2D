6039c50567ed9f5ad0ac0056e5849026b4d88404
Stein Karlsen <stein.vidar.karlsen@gmail.com>
2025-11-12 21:48:13 +0100

    Added project MonoGameLibrary

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace DungeonSlime;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        
     }
 
     protected override void Initialize()
@@ -24,8 +21,7 @@ public class Game1 : Game
     }
 
     protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    {        
 
         // TODO: use this.Content to load your game content here
     }
